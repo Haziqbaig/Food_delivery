1d82ecec171982772a3c2ce01f9ed009a04b51b9
Haziqbaig <91672781+Haziqbaig@users.noreply.github.com>
2024-05-08 20:42:24 +0500

    Pushed Cart

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -17,15 +17,27 @@ const StoreContextProvider = (props) => {
     const removeFromCart = (itemId) => {
         setCartItems((prev) => ({ ...prev, [itemId]: prev[itemId] - 1 }))
     }
-    useEffect(() => {
-        console.log("Cart Items", cartItems);
-    }, [cartItems])
+
+    const getTotalCartAmount = () => {
+        let totalAmount = 0;
+        for (const item in cartItems) {
+            if (cartItems[item] > 0) {
+                let itemInfo = food_list.find((product) => product._id === item)
+                totalAmount += itemInfo.price * cartItems[item];
+            }
+
+        }
+        return totalAmount;
+
+    }
+
     const contextValue = {
         food_list,
         cartItems,
         setCartItems,
         addToCart,
-        removeFromCart
+        removeFromCart,
+        getTotalCartAmount
     }
     return (
         <StoreContext.Provider value={contextValue}>
